3f57c0d5f8e42c6151100f27777ce1d9d01a4b45
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2021-11-10 18:36:22 -0300

    Melhorias feitas para corrigir erros no lint.

--- script.js
+++ script.js
@@ -1,86 +1,87 @@
-const buttonLogin = document.querySelector("#button-login");
-const emailLogin = document.querySelector("#email-login");
-const passwordLogin = document.querySelector("#password-login");
-const textArea = document.querySelector("#textarea");
-const counter = document.querySelector("#counter");
-const agreementCheckbox = document.querySelector("#agreement");
-const submitButton = document.querySelector("#submit-btn");
-const sectionFamily = document.querySelector("#section-family");
+const buttonLogin = document.querySelector('#button-login');
+const emailLogin = document.querySelector('#email-login');
+const passwordLogin = document.querySelector('#password-login');
+const textArea = document.querySelector('#textarea');
+const counter = document.querySelector('#counter');
+const agreementCheckbox = document.querySelector('#agreement');
+const submitButton = document.querySelector('#submit-btn');
+const sectionFamily = document.querySelector('#section-family');
 let numberOfCharacters = 0;
 let saveCount = textArea.maxLength;
 
 function checkTryber() {
-  if (emailLogin.value === "tryber@teste.com" && passwordLogin.value === "123456") {
-    window.alert("Olá, Tryber!");
+  if (emailLogin.value === 'tryber@teste.com' && passwordLogin.value === '123456') {
+    window.alert('Olá, Tryber!');
   } else {
-    window.alert("Email ou senha inválidos.");
+    window.alert('Email ou senha inválidos.');
   }
 }
 
-buttonLogin.addEventListener("click", checkTryber);
+buttonLogin.addEventListener('click', checkTryber);
 
-submitButton.addEventListener("click", function (event) {
-  const sectionInfo = document.querySelector("#section-info");
-  const inputName = document.querySelector("#input-name");
-  const inputLastname = document.querySelector("#input-lastname");
-  const inputEmail = document.querySelector("#input-email");
-  const selectHouse = document.querySelector("#house");
-  const radiosFamily = document.querySelectorAll(".radios-family");
+submitButton.addEventListener('click', function a (event) {
+  const sectionInfo = document.querySelector('#section-info');
+  const inputName = document.querySelector('#input-name');
+  const inputLastname = document.querySelector('#input-lastname');
+  const inputEmail = document.querySelector('#input-email');
+  const selectHouse = document.querySelector('#house');
+  const radiosFamily = document.querySelectorAll('.radios-family');
   event.preventDefault();
-  sectionInfo.innerHTML = "";
+  sectionInfo.innerHTML = '';
   sectionInfo.innerHTML += `<span>Nome: ${inputName.value} ${inputLastname.value}</span> <br />`;
   sectionInfo.innerHTML += `<span>Email: ${inputEmail.value}</span> <br />`;
   sectionInfo.innerHTML += `<span>Casa: ${selectHouse.value}</span> <br />`;
-  sectionFamily.innerHTML = "";
-  sectionFamily.innerHTML = "<p>Qual sua família?</p>";
+  sectionFamily.innerHTML = '';
+  sectionFamily.innerHTML = '<p>Qual sua família?</p>';
   for (let index = 0; index < radiosFamily.length; index += 1) {
     if (radiosFamily[index].checked === true) {
-      sectionFamily.innerHTML += "<span>Família: " + radiosFamily[index].value + "</span> <br />";
+      sectionFamily.innerHTML += `<span>Família: ${radiosFamily[index].value}</span> <br />`;
     }
   }
 });
 
-submitButton.addEventListener("click", function () {
-  const formLabelContent = document.querySelector("#form-label-content");
-  const checkboxContents = document.querySelectorAll(".subject");
-  formLabelContent.innerHTML = "";
-  formLabelContent.innerHTML = "<p>Qual conteúdo você está com mais vontade de aprender?</p>";
-  formLabelContent.innerHTML += "<span id='checkboxs-selected'>Matérias:  </span>";
-  const checboxsSelected = document.querySelector("#checkboxs-selected");
+submitButton.addEventListener('click', function b () {
+  const formLabelContent = document.querySelector('#form-label-content');
+  const checkboxContents = document.querySelectorAll('.subject');
+  formLabelContent.innerHTML = '';
+  formLabelContent.innerHTML = '<p>Qual conteúdo você está com mais vontade de aprender?</p>';
+  formLabelContent.innerHTML += `<span id='checkboxs-selected'>Matérias:  </span>`;
+  const checboxsSelected = document.querySelector('#checkboxs-selected');
   for (let index = 0; index < checkboxContents.length; index += 1) {
     if (checkboxContents[index].checked === true) {
-      checboxsSelected.innerHTML += "<span>" + checkboxContents[index].value + ', ' + "</span>";
+      checboxsSelected.innerHTML += `<span>${checkboxContents[index].value}, </span>`;
     }
   }
 });
 
-submitButton.addEventListener("click", function () {
-  const sectionEvaluation = document.querySelector("#section-evaluation");
-  const radiosEvaluation = document.querySelectorAll(".radios-evaluation");
-  sectionEvaluation.innerHTML = "";
-  sectionEvaluation.innerHTML = "<p>Como você avalia a Trybewarts?</p>";
+submitButton.addEventListener('click', function c () {
+  const sectionEvaluation = document.querySelector('#section-evaluation');
+  const radiosEvaluation = document.querySelectorAll('.radios-evaluation');
+  sectionEvaluation.innerHTML = '';
+  sectionEvaluation.innerHTML = '<p>Como você avalia a Trybewarts?</p>';
   for (let index = 0; index < radiosEvaluation.length; index += 1) {
     if (radiosEvaluation[index].checked === true) {
-      sectionEvaluation.innerHTML += "<span>Avaliação: " + radiosEvaluation[index].value + "</span> <br />";
+      sectionEvaluation.innerHTML += `<span>Avaliação: 
+      ${radiosEvaluation[index].value}</span> <br />`;
     }
   }
-  const sectionTextArea = document.querySelector("#section-textarea");
-  sectionTextArea.innerHTML = "";
-  sectionTextArea.innerHTML = "<p>Observações: </p>";
-  sectionTextArea.innerHTML += "<span>" + textArea.value + "</span>";
+  const sectionTextArea = document.querySelector('#section-textarea');
+  sectionTextArea.innerHTML = '';
+  sectionTextArea.innerHTML = '<p>Observações: </p>';
+  sectionTextArea.innerHTML += `<span>${textArea.value}</span>`;
 });
 
-agreementCheckbox.addEventListener("click", function () {
+agreementCheckbox.addEventListener('click', function d () {
   if (agreementCheckbox.checked === true) {
-    submitButton.removeAttribute("disabled");
+    submitButton.removeAttribute('disabled');
   } else {
-    submitButton.setAttribute("disabled", "disabled");
+    submitButton.setAttribute('disabled', 'disabled');
   }
 });
 
-textArea.addEventListener("keyup", function () {
+textArea.addEventListener('keyup', function e () {
   for (let index = 0; index < textArea.value.length; index += 1) {
-    numberOfCharacters += 1; 
+    numberOfCharacters += 1;
   }
   saveCount -= numberOfCharacters;
   counter.innerText = saveCount;
