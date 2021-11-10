c0c4154dc315c75bafb7f2e7defb3c4ff5f9cf02
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2021-11-10 00:09:12 -0300

    Requisito 21 - parte feito, ainda falta ser concluído.

--- script.js
+++ script.js
@@ -19,10 +19,51 @@ function checkTryber() {
 
 buttonLogin.addEventListener('click', checkTryber);
 
+submitButton.addEventListener('click', function(event) {
+  const sectionInfo = document.querySelector('#section-info');
+  const inputName = document.querySelector('#input-name');
+  const inputLastname = document.querySelector('#input-lastname');
+  const inputEmail = document.querySelector('#input-email');
+  const selectHouse = document.querySelector('#house');
+
+  //-----------------------------------------------------------
+  const sectionFamily = document.querySelector('#section-family');
+  const radioFrontend = document.querySelector('#radio-frontend');
+  const radioBackend = document.querySelector('#radio-backend');
+  const radioFullStack = document.querySelector('#radio-fullStack');
+
+  //-----------------------------------------------------------
+  const formLabelContent = document.querySelector('.form-label-content');
+  event.preventDefault();
+
+  //-----------------------------------------------------------
+  sectionInfo.innerHTML = '';
+  sectionInfo.innerHTML += '<span>Nome: ' + inputName.value + ' ' + inputLastname.value + '</span> <br />';
+  sectionInfo.innerHTML += '<span>Email: ' + inputEmail.value + '</span> <br />';
+  sectionInfo.innerHTML += '<span>Casa: ' + selectHouse.value + '</span> <br />';
+
+  //-----------------------------------------------------------
+  sectionFamily.innerHTML = '';
+  sectionFamily.innerHTML = '<p>Qual sua família?</p>';
+  if (radioFrontend.checked === true) {
+    sectionFamily.innerHTML += '<span>Família: ' + radioFrontend.value + '</span> <br />';
+
+  } else if (radioBackend.checked === true) {
+    sectionFamily.innerHTML += '<span>Família: ' + radioBackend.value + '</span> <br />';
+    
+  } else if (radioFullStack.checked === true) {
+    sectionFamily.innerHTML += '<span>Família: ' + radioFullStack.value + '</span> <br />';
+    
+  } else {
+    sectionFamily.innerHTML += '<span>Família: ' + '</span> <br />';
+  }
+  
+});
+
 agreementCheckbox.addEventListener('click', function() {
   if (agreementCheckbox.checked === true) {
     submitButton.removeAttribute('disabled');
-
+    
   } else {
     submitButton.setAttribute('disabled','disabled');
 
