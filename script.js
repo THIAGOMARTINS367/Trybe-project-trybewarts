a67b0aaf9f51820a0ebe709027922345fd815c41
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2021-11-09 23:09:26 -0300

    Requisitos de 18 a 20 feitos.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const buttonLogin = document.querySelector('#button-login');
 const emailLogin = document.querySelector('#email-login');
 const passwordLogin = document.querySelector('#password-login');
+const textArea = document.querySelector('#textarea');
+const counter = document.querySelector('#counter');
+const agreementCheckbox = document.querySelector('#agreement');
+const submitButton = document.querySelector('#submit-btn');
+
+let numberOfCharacters = 0;
+let saveCount = textArea.maxLength;
 
 function checkTryber() {
   if (emailLogin.value === 'tryber@teste.com' && passwordLogin.value === '123456') {
@@ -11,3 +18,24 @@ function checkTryber() {
 }
 
 buttonLogin.addEventListener('click', checkTryber);
+
+agreementCheckbox.addEventListener('click', function() {
+  if (agreementCheckbox.checked === true) {
+    submitButton.removeAttribute('disabled');
+
+  } else {
+    submitButton.setAttribute('disabled','disabled');
+
+  }
+});
+
+textArea.addEventListener('keyup', function() {
+  for (const iterator of textArea.value) {
+    numberOfCharacters += 1;
+  }
+  saveCount -= numberOfCharacters
+  counter.innerText = saveCount;
+  numberOfCharacters = 0;
+  saveCount = textArea.maxLength;
+  
+});
